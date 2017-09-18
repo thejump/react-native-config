9a960d1e2b1e85ffbb130b1259262311a2fb115f
jcharbo <jcharbo@users.noreply.github.com>
2017-09-18 10:31:40 -0500

    Update ReactNativeConfigPackage.java

--- android/src/main/java/com/lugg/ReactNativeConfig/ReactNativeConfigPackage.java
+++ android/src/main/java/com/lugg/ReactNativeConfig/ReactNativeConfigPackage.java
@@ -18,11 +18,7 @@ public class ReactNativeConfigPackage implements ReactPackage {
         );
     }
 
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
+  
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
